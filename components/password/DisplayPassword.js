5fbc85e3ebc96c3352256e71b6cea291bea8fba9
waseem <hello@waseemk.com>
2024-06-01 16:22:59 +0500

    Added Copied Password Feature

--- components/password/DisplayPassword.js
+++ components/password/DisplayPassword.js
@@ -1,11 +1,20 @@
-import React, { useState, memo, useMemo } from 'react';
-import { FiCopy } from 'react-icons/fi';
-import { useSelector } from 'react-redux';
+import React, {useState, memo, useMemo} from 'react';
+import {FiCopy} from 'react-icons/fi';
+import {useSelector} from 'react-redux';
 
 const DisplayPassword = () => {
+  const [isCopied, setCopied] = useState(false);
   const password = useSelector((store) => store.password.password);
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(password);
+    navigator.clipboard
+      .writeText(password)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000); // Reset isCopied after 2 seconds
+      })
+      .catch((err) => {
+        console.error('Failed to copy: ', err);
+      });
   };
 
   return (
@@ -19,7 +28,8 @@ const DisplayPassword = () => {
       >
         {password}
       </p>
-      <div className="text-3xl text-limeGreen transition-colors duration-150 ease-in hover:text-white">
+      <div className="flex items-center gap-5 text-3xl  text-limeGreen transition-colors duration-150 ease-in hover:text-white">
+        {isCopied ? <p className="text-2xl transition-all duration-100">Copied</p> : ''}
         <FiCopy />
       </div>
     </div>
